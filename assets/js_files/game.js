1b79a69a9dd77dd08bbc2e36b0138a605962a0b1
fene20 <feeneyk@gmail.com>
2021-05-07 20:29:45 +0000

    Add Code Institute Challenge to repo

---
+++ assets/js_files/game.js
@@ -0,0 +1,13 @@
+function boxClicked(event){
+    if (this.style.backgroundColor ===  "orange"){
+        this.style.backgroundColor = "green";
+    }else{
+        this.style.backgroundColor = "orange";
+    }
+}
+
+let boxes = document.getElementsByClassName("box");
+
+for(i = 0, l = boxes.length; i < l; i++) {
+    boxes[i].addEventListener('click', boxClicked);
+}  
